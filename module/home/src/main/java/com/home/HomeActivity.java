6bbbfd6ca0b2e359c21d0f28f3a9640c1f7f26ff
strawberryhaoci <3118749593@qq.com>
2023-04-17 19:00:38 +0800

    add navigation

--- module/home/src/main/java/com/home/HomeActivity.java
+++ module/home/src/main/java/com/home/HomeActivity.java
@@ -1,10 +1,19 @@
 package com.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.util.PermissionUtil;
+
+import static android.content.ContentValues.TAG;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,5 +22,14 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         PermissionUtil.askPermission(this);
+        //配置导航控件
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_1,R.id.nav_2,R.id.nav_3
+        ).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navView,navController);
     }
 }
---
+++ module/home/src/main/java/com/util/ClickEvent.java
@@ -0,0 +1,20 @@
+package com.util;/*
+ *name: AIIllustration
+ *description:
+ */
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * @program: AIIllustration
+ * @description: click item event
+ */
+public class ClickEvent {
+    public void toHome(View V){
+        Log.d(TAG, "toHome: ");
+    }
+}
